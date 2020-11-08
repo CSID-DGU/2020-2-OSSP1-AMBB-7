bd3c90a2b906ec58d8c473c0f17ea0cb843e6809
KKhajiit <junesup0402@gmail.com>
2020-11-08 22:45:05 +0900

    Rotation and Zoom In/Out

---
+++ part_Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.XR.WSA.Input;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform cameraOrbit;
+    public Transform target;
+
+    void Start()
+    {
+        cameraOrbit.position = target.position;
+    }
+
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x, transform.rotation.y, 0);
+
+        transform.LookAt(target.position);
+    }
+}
