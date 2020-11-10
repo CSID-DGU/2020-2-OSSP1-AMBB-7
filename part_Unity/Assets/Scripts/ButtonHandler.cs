688548510c3f09176e28aee4b864c2d2d2e0a229
myeungve <myeungve@naver.com>
2020-11-11 03:24:01 +0900

    implement output function

---
+++ part_Unity/Assets/Scripts/ButtonHandler.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Dynamic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+
+public class ButtonHandler : MonoBehaviour
+{
+    Button button;
+    Button button2;
+    public GameObject controlObject;
+    
+    public void OnClickButton()
+    {
+        Writetxt();
+        Capturejpg();
+
+        controlObject.SetActive(true);
+        button2 = controlObject.GetComponent<Button>();
+        button2.onClick.AddListener(ButtonHidden);
+    }
+
+    public void ButtonHidden()
+    {
+        controlObject.SetActive(false);
+    }
+
+    public void Writetxt()
+    {
+        //File.Create("Assets/Resources/3DmodelInfo.txt");
+        StreamWriter sw = new StreamWriter("Assets/Output/3DmodelInfo.txt");
+        sw.WriteLine("Add 3d object's information!");
+        sw.Close();
+    }
+
+    public void Capturejpg()
+    {
+        Texture2D tex = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, true);
+        tex.ReadPixels(new Rect(0f, 0f, Screen.width, Screen.height), 0, 0, true);
+        tex.Apply();
+        byte[] bytes = tex.EncodeToPNG();
+        File.WriteAllBytes("Assets/Output/3DmodelCapture.jpg", bytes);
+        DestroyImmediate(tex);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        controlObject = GameObject.Find("Button2");
+        controlObject.SetActive(false);
+        button = GetComponent<Button>();
+        button.onClick.AddListener(OnClickButton);
+    }
+}
